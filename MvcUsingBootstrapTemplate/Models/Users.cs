1b9b76031c3b1971899e542abc2458f2ea990103
Aditya <Aditya@DESKTOP-350880K>
2018-11-05 15:58:57 +0530

    first commit without library

---
+++ MvcUsingBootstrapTemplate/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcUsingBootstrapTemplate.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        [Column("AdAccount_Domain")]
+        public string AdAccountDomain { get; set; }
+        [Column("AdAccount_Name")]
+        public string AdAccountName { get; set; }
+    }
+}
---
+++ MvcUsingBootstrapTemplate/Models/northwindtradersContext.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MvcUsingBootstrapTemplate.Models
+{
+    public partial class northwindtradersContext : DbContext
+    {
+        public northwindtradersContext()
+        {
+        }
+
+        public northwindtradersContext(DbContextOptions<northwindtradersContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<Employees> Employees { get; set; }
+        public virtual DbSet<EmployeeTerritories> EmployeeTerritories { get; set; }
+        public virtual DbSet<OrderDetails> OrderDetails { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<Region> Region { get; set; }
+        public virtual DbSet<Shippers> Shippers { get; set; }
+        public virtual DbSet<Suppliers> Suppliers { get; set; }
+        public virtual DbSet<Territories> Territories { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-PTAUQID;Database=northwindtraders;User ID=sa;Password=shezar@123;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customers>(entity =>
+            {
+                entity.Property(e => e.CustomerId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.HasIndex(e => e.ReportsTo);
+
+                entity.HasOne(d => d.ReportsToNavigation)
+                    .WithMany(p => p.InverseReportsToNavigation)
+                    .HasForeignKey(d => d.ReportsTo)
+                    .HasConstraintName("FK_Employees_Employees");
+            });
+
+            modelBuilder.Entity<EmployeeTerritories>(entity =>
+            {
+                entity.HasKey(e => new { e.EmployeeId, e.TerritoryId })
+                    .ForSqlServerIsClustered(false);
+
+                entity.HasIndex(e => e.TerritoryId);
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany(p => p.EmployeeTerritories)
+                    .HasForeignKey(d => d.EmployeeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_EmployeeTerritories_Employees");
+
+                entity.HasOne(d => d.Territory)
+                    .WithMany(p => p.EmployeeTerritories)
+                    .HasForeignKey(d => d.TerritoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_EmployeeTerritories_Territories");
+            });
+
+            modelBuilder.Entity<OrderDetails>(entity =>
+            {
+                entity.HasKey(e => new { e.OrderId, e.ProductId });
+
+                entity.HasIndex(e => e.ProductId);
+
+                entity.Property(e => e.Quantity).HasDefaultValueSql("((1))");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_Details_Orders");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_Details_Products");
+            });
+
+            modelBuilder.Entity<Orders>(entity =>
+            {
+                entity.HasIndex(e => e.CustomerId);
+
+                entity.HasIndex(e => e.EmployeeId);
+
+                entity.HasIndex(e => e.ShipVia);
+
+                entity.Property(e => e.Freight).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .HasConstraintName("FK_Orders_Customers");
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.EmployeeId)
+                    .HasConstraintName("FK_Orders_Employees");
+
+                entity.HasOne(d => d.ShipViaNavigation)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.ShipVia)
+                    .HasConstraintName("FK_Orders_Shippers");
+            });
+
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasIndex(e => e.CategoryId);
+
+                entity.HasIndex(e => e.SupplierId);
+
+                entity.Property(e => e.ReorderLevel).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitPrice).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsInStock).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_Products_Categories");
+
+                entity.HasOne(d => d.Supplier)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.SupplierId)
+                    .HasConstraintName("FK_Products_Suppliers");
+            });
+
+            modelBuilder.Entity<Region>(entity =>
+            {
+                entity.HasKey(e => e.RegionId)
+                    .ForSqlServerIsClustered(false);
+
+                entity.Property(e => e.RegionId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Territories>(entity =>
+            {
+                entity.HasKey(e => e.TerritoryId)
+                    .ForSqlServerIsClustered(false);
+
+                entity.HasIndex(e => e.RegionId);
+
+                entity.Property(e => e.TerritoryId).ValueGeneratedNever();
+
+                entity.HasOne(d => d.Region)
+                    .WithMany(p => p.Territories)
+                    .HasForeignKey(d => d.RegionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Territories_Region");
+            });
+        }
+    }
+}
